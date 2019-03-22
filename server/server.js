cedd008986fcb011328cb8bbdc93a425b9a57dc5
Jamshaid Azeem <jamshaidazeem@gmail.com>
2019-03-22 20:55:28 +0500

    express and body-parser packages added

--- server/server.js
+++ server/server.js
@@ -1,3 +1,12 @@
+const express = require('express');
+const body-parser = require('body-parser');
+
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
+
+const app = express();
+
+app.listen(3000, 'localhost', () => {
+    console.log('listening on port 3000...');
+});
