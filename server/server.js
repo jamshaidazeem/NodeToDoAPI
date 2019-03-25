3cfa8ff63bc03609673012cad9ba375f478be0d2
Jamshaid Azeem <jamshaidazeem@gmail.com>
2019-03-25 17:55:02 +0500

    Todo find by id end point added

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
@@ -15,7 +16,7 @@ app.post('/api/todo', (req, res) => {
     model.save().then((doc) => {
         res.status(200).send(doc);
     }, (err) => {
-        res.status(400).send(err.message);
+        res.status(400).send(err);
     });
 });
 
@@ -23,7 +24,18 @@ app.get('/api/todos', (req, res) => {
     Todo.find().then((models) => {
         res.send({data: models});
     }, (err) => {
-        res.status(400).send(err.message);
+        res.status(400).send(err);
+    });
+});
+
+app.get('/api/todos/:id', (req, res) => {
+    const id = req.params.id;
+    if (!ObjectID.isValid(id)) return res.status(400).send('id is not valid!');
+    Todo.findById(id).then((model) => {
+        if (!model) return res.status(404).send('no object found matching this id!');
+        res.status(200).send({data: model});
+    }, (err) => {
+        res.status(404).send(err);
     });
 });
 
