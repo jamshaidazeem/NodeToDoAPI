114fed5ec3fc4496b19c2b51112c570a4a151097
Jamshaid Azeem <jamshaidazeem@gmail.com>
2019-03-26 14:43:37 +0500

    patch updated endpoint added

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+const _ = require('lodash');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
@@ -49,6 +50,40 @@ app.delete('/api/todos/:id', (req, res) => {
         res.status(404).send(err);
     });
 });
+
+// update using patch method
+// user send us id of document and in body send us props to update
+// we can't let user update every property so we use lodash pick method to pick only valid props
+// if task is completed than we update completed time stamp and vice versa
+// we than use findbyidandupdate method and mongoose update operators 
+
+app.patch('/api/todos/:id', (req, res) => {
+    const id = req.params.id;
+    if (!ObjectID.isValid(id)) return res.status(400).send('id is not valid!');
+
+    let body = _.pick(req.body, ['text', 'completed']);
+    if (_.isBoolean(body.completed)) { 
+        if (body.completed === true) {
+            body.completedAt = new Date().getTime();
+        } else {
+            body.completedAt = null;
+        }  
+    } else {
+       body = _.omit(body, ["completed"]); // in case completed is not valid we omit it
+    } 
+
+    Todo.findByIdAndUpdate(id,{
+        $set : body
+    }, {
+        new : true // send back updated object
+    }).then((model) => {
+        if (!model) return res.status(404).send('no object found matching this id!');
+        res.status(200).send({ data: model });
+    }, (err) => {
+        res.status(404).send(err);
+    });
+});
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, 'localhost', () => {
