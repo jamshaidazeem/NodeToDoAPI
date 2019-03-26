9fc856c2e700889d0bff71afb4249f558664d345
Jamshaid Azeem <jamshaidazeem@gmail.com>
2019-03-26 12:44:14 +0500

    remove todo api added

--- server/server.js
+++ server/server.js
@@ -39,6 +39,17 @@ app.get('/api/todos/:id', (req, res) => {
     });
 });
 
+app.delete('/api/todos/:id', (req, res) => {
+    const id = req.params.id;
+    if (!ObjectID.isValid(id)) return res.status(400).send('id is not valid!');
+    Todo.findByIdAndDelete(id).then((model) => {
+        if (!model) return res.status(404).send('no object found matching this id!');
+        res.status(200).send({ data: model });
+    }, (err) => {
+        res.status(404).send(err);
+    });
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, 'localhost', () => {
     console.log(`listening on port ${port}...`);
